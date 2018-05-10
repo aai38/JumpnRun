b31626986904f96ddca90c5c7844c127add31cf9
Karolin <karolin.bartlmae@uni-ulm.de>
2018-05-10 21:40:55 +0200

    Controller for Touch and Keys

---
+++ Assets/Scripts/Controll.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controll : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed;
+    public bool moveright;
+    public bool moveleft;
+    public Vector2 jumpHeight;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+        if ( Input.GetKeyDown(KeyCode.UpArrow))  //makes player jump
+        {
+
+            rb.AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchController : MonoBehaviour
+{
+    private Controll player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<Controll>();
+    }
+
+    public void LeftArrow()
+    {
+        player.moveright = false;
+        player.moveleft = true;
+    }
+    public void RightArrow()
+    {
+        player.moveright = true;
+        player.moveleft = false;
+    }
+    public void ReleaseLeftArrow()
+    {
+        player.moveleft = false;
+    }
+    public void ReleaseRightArrow()
+    {
+        player.moveright = false;
+
+    }
+
+}
