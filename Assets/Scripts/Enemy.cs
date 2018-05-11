111bae2645bc84505a3c2ee47f93d9a8322e113d
tea22 <35063534+tea22@users.noreply.github.com>
2018-05-11 19:50:15 +0200

    Enemy added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+
+
+    public float speed;
+
+    private Transform target;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
+
+
