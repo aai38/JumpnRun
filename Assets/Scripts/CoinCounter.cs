9f5a39542f0bc23110c44aec38f0f2e13246097b
tea22 <35063534+tea22@users.noreply.github.com>
2018-05-21 23:20:59 +0200

    Added some stupid WTF collectables

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCounter : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		CollectablesText.coinAmount += 1;
+		Destroy (gameObject);
+	}
+	
+}
---
+++ Assets/Scripts/CollectablesSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablesSpawner : MonoBehaviour {
+
+    public GameObject collectables;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 5f;
+    float nextSpawn = 0.0f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(609f, 242f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(collectables, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+}
