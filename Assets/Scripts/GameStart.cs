27d68b5d4b00f9f6cb48962680c4d33b94ae59c6
susannebair <susanne.bair@uni-ulm.de>
2018-05-23 00:13:54 +0200

    Added Animation and Pause Function
    There's now an animation to initiate the game and you can pause it by clicking the Pause Button or pressing P. The shortcut works perfectly but not the Play Button.

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour {
+
+    /*public GameObject go;
+
+	// Use this for initialization
+	void Start () {
+		go = GameObject.FindGameObjectWithTag("game_start");
+
+
+
+        //StartCoroutine(ShowAtBeginning());
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine(ShowMessage());
+	}
+
+	IEnumerator ShowMessage()
+	{
+        go.SetActive(true);
+		yield return new WaitForSeconds(3);
+        go.SetActive(false);
+	}*/
+
+    public Text message;
+	void Start()
+	{
+		Invoke("DisableText", 3f);//invoke after 5 seconds
+	}
+	void DisableText()
+	{
+		message.enabled = false;
+	}
+}
