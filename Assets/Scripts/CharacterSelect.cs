50cba13be47d6d48639144f004a778b0041a6835
susannebair <susanne.bair@uni-ulm.de>
2018-05-29 20:33:05 +0200

    Started Character Selection

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour {
+
+    public GameObject[] players;
+
+	// Use this for initialization
+	void Start () {
+		int selected = PlayerPrefs.GetInt("selectedCharacter");
+		Instantiate(players[selected], Vector2.zero, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
