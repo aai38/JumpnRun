cc84b807fca8624d321ab3d8ab0784135da15358
tea22 <35063534+tea22@users.noreply.github.com>
2018-06-07 11:15:32 +0200

    Collectables dissapear after some sec.

---
+++ Assets/Scripts/DissapearCollectables.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissapearCollectables : MonoBehaviour
+{
+
+    float lifeTime = 5.0f;
+
+    void Awake()
+    { Destroy(gameObject, lifeTime); }
+}
